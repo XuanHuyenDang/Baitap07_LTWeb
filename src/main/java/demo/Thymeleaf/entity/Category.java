5749c75a565710f30c17a083dda96a6389bbd962
XuanHuyenDang <dangxuanhuyen2005@gmail.com>
2025-09-18 09:36:34 +0700

    Lan1

---
+++ src/main/java/demo/Thymeleaf/entity/Category.java
@@ -0,0 +1,42 @@
+package demo.Thymeleaf.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    private String description;
+
+    // Getter & Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
